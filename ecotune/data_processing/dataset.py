59b0c1183861462418b6a4c7db307310462e9b25
Ahmad Khan <163046609+ahmadkhan100@users.noreply.github.com>
2024-09-16 00:31:00 -0400

    Create dataset.py

---
+++ ecotune/data_processing/dataset.py
@@ -0,0 +1,26 @@
+import torch
+from torch.utils.data import Dataset
+
+class EcoTuneDataset(Dataset):
+    def __init__(self, data, tokenizer, max_length):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        encoding = self.tokenizer(
+            item['text'],
+            truncation=True,
+            max_length=self.max_length,
+            padding='max_length',
+            return_tensors='pt'
+        )
+        return {
+            'input_ids': encoding['input_ids'].flatten(),
+            'attention_mask': encoding['attention_mask'].flatten(),
+            'labels': torch.tensor(item['label'])
+        }
